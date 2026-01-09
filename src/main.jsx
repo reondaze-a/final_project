5e514f85ec20394fb0b391dc3da2bc1074f60259
Abraham Efraim <75068289+reondaze-a@users.noreply.github.com>
2026-01-09 17:06:27 -0500

    Update main.jsx
    Removed "basename" from HashRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename="/final_project">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
